e11a1996d19c059410f2f693d58fa1442a7acb84
bbawuah <brianbawuah@icloud.com>
2021-10-11 08:25:15 +0200

    add Buffer to output globals

--- rollup.config.js
+++ rollup.config.js
@@ -41,7 +41,10 @@ export default {
     sourcemap: true,
     format: 'iife',
     name: 'app',
-    file: 'public/build/bundle.js'
+    file: 'public/build/bundle.js',
+    globals: {
+      buffer: 'Buffer'
+    }
   },
   plugins: [
     svelte({
