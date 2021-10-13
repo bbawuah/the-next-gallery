955af7d6f0c51d6da542bf854ad11c7bbe8fc487
bbawuah <brianbawuah@icloud.com>
2021-10-13 21:59:21 +0200

    adjusting the speed

--- src/components/webgl/Events/Events.ts
+++ src/components/webgl/Events/Events.ts
@@ -21,7 +21,7 @@ export class Events {
     this._left = false;
     this._right = false;
 
-    this.walkingSpeed = 3;
+    this.walkingSpeed = 3.3;
 
     playerIsInScene.subscribe(value => {
       this.playerIsInScene = value;
