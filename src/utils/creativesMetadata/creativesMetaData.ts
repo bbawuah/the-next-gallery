3bcc87156565b69ea184ebfcaada3a52a083efa5
Brian Bawuah <36267063+bbawuah@users.noreply.github.com>
2021-10-26 20:59:45 +0200

    add creatives meta

---
+++ src/utils/creativesMetadata/creativesMetaData.ts
@@ -0,0 +1,105 @@
+export const metadata = [
+  {
+    name: 'Jamil Mensah',
+    instagram: 'https://www.instagram.com/men.sah/?hl=nl'
+  },
+  {
+    name: 'Daliss',
+    instagram: 'https://www.instagram.com/dalisshimself/?hl=nl'
+  },
+  {
+    name: 'Denitio Ravenberg'
+  },
+  {
+    name: 'Emmanuel Owusu',
+    instagram: 'https://www.instagram.com/eaowusu/?hl=nl'
+  },
+  {
+    name: 'Churchbwoygram',
+    instagram: 'https://www.instagram.com/churchbwoygram/?hl=nl'
+  },
+  {
+    name: 'Jaysi',
+    instagram: 'https://www.instagram.com/jaysimn/?hl=nl'
+  },
+  {
+    name: 'Ronald Gyamfi',
+    instagram: 'https://www.instagram.com/mr.gyamfii/?hl=nl'
+  },
+  {
+    name: 'Meester Kwame',
+    instagram: 'https://www.instagram.com/meester_kwame/?hl=nl'
+  },
+  {
+    name: 'Kenneth Aidoo',
+    instagram: 'https://www.instagram.com/kennethjames/?hl=nl'
+  },
+  {
+    name: 'Terry Afram',
+    instagram: 'https://www.instagram.com/terryquasi/?hl=nl'
+  },
+  {
+    name: 'Crystalina Agyemang',
+    instagram: 'https://www.instagram.com/icrystalina/?hl=nl'
+  },
+  {
+    name: 'Mirella Agyemann',
+    instagram: 'https://www.instagram.com/mirellaabn/?hl=nl'
+  },
+  {
+    name: 'Les Adu',
+    instagram: 'https://www.instagram.com/les_adu/?hl=nl'
+  },
+  {
+    name: 'Junnior Appiah',
+    instagram: 'https://www.instagram.com/juniorappiah/?hl=nl'
+  },
+  {
+    name: 'Darryl Amankwah',
+    instagram: 'https://www.instagram.com/darrylamankwah/?hl=nl'
+  },
+  {
+    name: 'Daniel Bonsu',
+    instagram: 'https://www.instagram.com/bonsu.1/?hl=nl'
+  },
+  {
+    name: 'Emmanuel Ohene Boafo',
+    instagram: 'https://www.instagram.com/eoboafo/?hl=nl'
+  },
+  {
+    name: 'Shaqueena Vrede',
+    instagram: 'https://www.instagram.com/shaneequa.vrede/?hl=nl'
+  },
+  {
+    name: 'Samantha Pollack',
+    instagram: 'https://www.instagram.com/samanthapollack/?hl=nl'
+  },
+  {
+    name: 'Soraja Haakmat',
+    instagram: 'https://www.instagram.com/shanillia26/?hl=nl'
+  },
+  {
+    name: 'Othnel "N64" Goodett',
+    instagram: 'https://www.instagram.com/n64gram/?hl=nl'
+  },
+  {
+    name: 'Porchia Azieku',
+    instagram: 'https://www.instagram.com/porchiaab/?hl=nl'
+  },
+  {
+    name: 'Kwame Boakye Agyeman',
+    instagram: 'https://www.instagram.com/kwameagyemann/?hl=nl'
+  },
+  {
+    name: 'TonnyGha',
+    instagram: 'https://www.instagram.com/tonny.gha/?hl=nl'
+  },
+  {
+    name: 'BRANKO MC',
+    instagram: 'https://www.instagram.com/brankomc/?hl=nl'
+  },
+  {
+    name: 'Eben Badu',
+    instagram: 'https://www.instagram.com/ebenbadu/?hl=nl'
+  }
+];
