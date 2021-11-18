9f782e316bf01c2bdcb23eaf8dc17fdca27bc8ad
Brian Bawuah <36267063+bbawuah@users.noreply.github.com>
2021-11-18 17:44:43 +0100

    add serviceworker

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,6 @@ import * as THREE from 'three';
 
 window.THREE = THREE;
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('./service-worker.js', {scope: '.'});
-}
-
 export default new App({
   target: document.body
 });
